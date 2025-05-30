490b4ddc71dedf1d620e5343e578944f165ad866
joseph <joeadewunmi55@gmail.com>
2025-05-30 09:17:20 +0100

    Update user count from 900+ to 1000+ users

--- src/components/templates/CouncilTemplate.tsx
+++ src/components/templates/CouncilTemplate.tsx
@@ -86,7 +86,7 @@ const CouncilTemplate = ({ councilName, councilSlug, faqs }: CouncilTemplateProp
                 >
                   Appeal now
                 </TrackingButton>
-                <p className="mt-2 text-sm text-primary/70 font-bold">Used by 900+ users </p>
+                <p className="mt-2 text-sm text-primary/70 font-bold">Used by 1000+ users </p>
                 <p className="mt-1 text-sm text-primary/70">Requires ChatGPT login</p>
               </div>
             </div>
