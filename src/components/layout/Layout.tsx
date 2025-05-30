d257b04968163debf5d4753e98f300d854198242
joseph <joeadewunmi55@gmail.com>
2025-05-30 13:29:09 +0100

    Add Hotjar tracking script to Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ScrollToTop from "../utils/ScrollToTop";
@@ -24,6 +25,21 @@ const Layout = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
+      <Helmet>
+        {/* Hotjar Tracking Code */}
+        <script>
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6419397,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </script>
+      </Helmet>
       <ScrollToTop />
       <Navbar />
       <main className={`flex-grow ${isLoading ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}>
