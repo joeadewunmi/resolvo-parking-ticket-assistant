d08c5a31f53b3b9a34e238a5ec5ca18d4f57ff5b
joseph <joeadewunmi55@gmail.com>
2025-06-15 23:57:54 +0100

    fix(images): resolve cropping by using object-contain

--- src/components/ui/Image.tsx
+++ src/components/ui/Image.tsx
@@ -29,7 +29,6 @@ const Image: React.FC<ImageProps> = ({
         style={{
           width: '100%',
           height: '100%',
-          objectFit: 'cover',
           position: 'absolute',
           top: 0,
           left: 0,
